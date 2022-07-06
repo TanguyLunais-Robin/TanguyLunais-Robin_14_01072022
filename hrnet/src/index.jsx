44e6b44544f36ca404670ce992e19252fc58b411
TanguyLunais-Robin <tanguy.lunais-robin@hotmail.fr>
2022-07-06 11:22:38 +0200

    Mise en place route + pages

--- hrnet/src/index.jsx
+++ hrnet/src/index.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+//Components
+import Header from './components/Header';
+
+//Page
+import CreateEmployee from "./pages/CreateEmployee"
+import CurrentEmployees from "./pages/CurrentEmployees"
+
+//Styles
+import './styles/index.css';
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Header />
+        <Switch>
+          <Route exact path = "/" component = { CreateEmployee } />
+          <Route exact path = "/current-employees" component = { CurrentEmployees } />
+        </Switch>
+      </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
